bb0912b12fe623c0dba9eb84a360b66ae5e8044d
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-07-12 14:47:00 +0000

    homescreen navigation, added two more components

---
+++ HomePage/Homescreen.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, View, ScrollView } from "react-native";
+import Header from "./Header";
+import OurStory from "./OurStory";
+import Products from "./Products";
+
+function HomeScreen({navigation}){
+    return(
+    <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scroll}>
+        <Header navigation={navigation}/>
+        </ScrollView>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#FFFFFF'
+    },
+
+    scroll:{
+        flexGrow: 1
+    }
+})
+
+export default HomeScreen;
